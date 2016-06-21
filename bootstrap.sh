cc906cd9333c20330f2876d6c2ec41bbf997fcbc
Yury Vovk <vovk.yury@me.com>
2016-06-22 01:07:57 +0300

    + detect target at bootstrap build stage

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 echo "Looking for ObjFW..."
-if ! which objfw-compile >/dev/null 2>&1; then
+TRIPLE="$(gcc -dumpmachine)"
+OBJFW_COMPILE="$TRIPLE-objfw-compile"
+if ! which $OBJFW_COMPILE >/dev/null 2>&1; then
 	echo
 	echo "ERROR: You need ObjFW in order to boostrap bake!"
 	echo "       Get it from https://webkeks.org/objfw/ and install it!"
@@ -10,15 +12,16 @@ echo "Found objfw-compile!"
 
 echo
 echo "Bootstrapping bake using objfw-compile:"
-objfw-compile -Wall -Werror -g -Isrc -Isrc/exceptions -o bootstrap_bake \
+$OBJFW_COMPILE -Wall -Werror -g -Isrc -Isrc/exceptions -o bootstrap_bake \
 	src/*.m src/exceptions/*.m || exit 1
 rm src/*.o src/exceptions/*.o
 
 echo
 echo "Generating ingredient file for ObjFW..."
+OBJFW_CONFIG="$TRIPLE-objfw-config"
 mkdir -p ingredients
 ./bootstrap_bake --produce-ingredient \
-	`objfw-config --cppflags --objcflags --libs` \
+	`$OBJFW_CONFIG --cppflags --objcflags --libs` \
 	>ingredients/objfw.ingredient || exit 1
 echo "Successfully generated!"
 
